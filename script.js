8e45f616a02d4c686b652cce1c3f0ca22ba9541a
DavidHearl <david.hearl@outlook.com>
2025-05-09 17:59:49 +1000

    completed rough layout

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {
+    const button = document.getElementById("backToTop");
+    if (document.documentElement.scrollTop > 200) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
